b292f0a5f12386fb74ef1d3c061a3c16e588d448
Romi Julianto <romyjulians@gmail.com>
2024-01-07 17:24:28 +0700

    feat: integrated socket.io and emitting events

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const { createServer } = require('node:http');
 const dotenv = require("dotenv").config()
 const { join } = require('node:path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT;
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'src', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
 });
 
 server.listen(PORT, () => {
