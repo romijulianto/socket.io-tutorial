bec28079c687dffb64f62a83399281802ff9454a
Romi Julianto <romyjulians@gmail.com>
2024-01-07 18:02:27 +0700

    feat: disconnect socket if not used

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ const { Server } = require('socket.io');
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT;
-const io = new Server(server);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'src', 'index.html'));
 });
 
 io.on('connection', (socket) => {
+    io.emit("connected true")
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
 });
+
+io.on('disconnection', (socket) => {
+    io.emit("disconnect true")
+});
+
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
